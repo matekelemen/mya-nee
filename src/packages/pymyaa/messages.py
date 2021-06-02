61e1db328911879ef6faef297a9caf65762da0c0
Mate Kelemen <mate.kelemen.12@gmail.com>
2021-06-02 14:22:59 +0200

    delete old implementation

--- src/packages/pymyaa/messages.py
+++
@@ -1,7 +0,0 @@
-# ------------------------------------------------
-# STATUS MESSAGES
-# ------------------------------------------------
-
-MESSAGE_LOG_ON = r"Ohayō min'na!"
-
-MESSAGE_LOG_OUT = r"Oyasuminasai, minasan!"
